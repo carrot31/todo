fe38302ed20cffa78aa6bfb1308afe38b5a557fa
carrot31 <jungwon0508@naver.com>
2022-08-10 02:26:51 +0900

    [09] v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="name">{{ name }}</div>
-	<button class="btn btn-primary">Click</button>
+  <h1 v-bind:class='nameClass'>
+    {{ name }}
+  </h1>
+  <input :type="type" :value="name"/>
+	<button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 
 <script>
+import {ref} from 'vue';
 
 export default {
   setup() {
-		const name = 'Kossie Coder';
+		const name = ref('Kossie');
+    const type = ref('number');
+    const nameClass = ref('name')
 
-		// const greeting = name => {
-		// 	return 'Hello, ' + name;
-		// };
-
-		// const greet = greeting(name);
+    const updateName = () => {
+      name.value = 'Mandu'
+      type.value = 'text'
+      nameClass.value = 'name'
+    }
 
 		return {
+      nameClass,
+      type,
 			name,
+      updateName,
 			// greet,
 		};
 	},
