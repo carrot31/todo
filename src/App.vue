8c2bb2a383c488f79521ec8656543762501d906a
carrot31 <jungwon0508@naver.com>
2022-08-11 05:31:39 +0900

    [23] computed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 
   <div class="container">
+    <h4>count: {{count}}</h4>
+    <h4>doubleCount: {{doubleCount}}</h4>
+    <h4>doubleCountMethod: {{doubleCountMethod()}}</h4>
+    <button @click="count++">Click</button>
     <h1>To-Do List</h1>
     <TodoSimpleForm @add-todo="addTodo" />
       <div v-if="!todos.length">
@@ -15,7 +19,7 @@
 </template>
 
 <script>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -35,16 +39,31 @@ export default {
         };
         const toggleTodo = (index) => {
             todos.value[index].completed = !todos.value[index].completed
-        }
+        };
         const deleteTodo = (index) => {
             todos.value.splice(index, 1);
         };
+        const count = ref(1);
+        //함수 vs computed 
+        //computed는 인자로 받아올 수 x => computed안에 ref가 실행되거나 값이 변경될때만 변경됌 
+        //computed는 값을 캐쉬함! 
+        const doubleCount = computed(()=>{
+          console.log('computed')
+          return count.value*2
+        })
+        const doubleCountMethod = () =>{
+          console.log('method')
+          return count.value*2;
+        }
         return {
             todoStyle,
             todos,
             addTodo,
             toggleTodo,
             deleteTodo,
+            count,
+            doubleCount,
+            doubleCountMethod,
         };
     },
     components: { TodoSimpleForm, TodoList }
